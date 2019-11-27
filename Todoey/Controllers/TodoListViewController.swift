95f00ec31566c7c29271fcb691b38b1c800657d5
FernandoLuizRosa <f72@outlook.com>
2019-11-27 13:32:38 -0300

    Persist data using Codable protocol and Plist.

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -9,33 +9,34 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
-
-    //var itemArray = ["Do Stuff", "Do another Stuff", "Do even more Stuff"]
         
     var itemArray = [Item]()
     
-    let defaults = UserDefaults.standard
+    //let defaults = UserDefaults.standard
+    
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var n = 0
         
-        for _ in 1...35 {
-            n += 1
-            let newItem = Item()
-            newItem.title = "Bolacha \(n)"
-            itemArray.append(newItem)
-        }
         
-         
-//        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+//        var n = 0
+//
+//        for _ in 1...35 {
+//            n += 1
+//            let newItem = Item()
+//            newItem.title = "Bolacha \(n)"
+//            itemArray.append(newItem)
+//        }
+        
+//        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
 //            itemArray = items
 //        }
         
-        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
-            itemArray = items
-        }
+        loadItems()
+
         
     }
     
@@ -46,11 +47,7 @@ class TodoListViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-                
-          /*
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
-        cell.textLabel?.text = message
-        */
+
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
         
@@ -60,50 +57,25 @@ class TodoListViewController: UITableViewController {
         
         cell.accessoryType = item.done ? .checkmark : .none
         
-        // isto é verdade ? Sim : Não
-        /* O mesmo acima usando o Ternary Operator
-        if item.done == true {
-            cell.accessoryType = .checkmark
-        } else {
-            cell.accessoryType = .none
-        }*/
-        
-        
         return cell
     }
     
     //MARK: - TableView Delegate Methods
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //print(itemArray[indexPath.row])
-        /*
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-            
-        } else {
-            
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }*/
                 
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
-        /* a linha acima substitui o código abaixo
-        if itemArray[indexPath.row].done == true {
-            
-            itemArray[indexPath.row].done = false
-            
-        } else {
-            
-            itemArray[indexPath.row].done = true
-        }*/
-        
-        tableView.reloadData()
+        saveItems()
+                
+        //tableView.reloadData()
         
         tableView.deselectRow(at: indexPath, animated: true)
         
     }
     //MARK: - Add New Items
+    
+    
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
@@ -118,13 +90,9 @@ class TodoListViewController: UITableViewController {
             newItem.title = textField.text!
             self.itemArray.append(newItem)
             
-            //self.itemArray.append(textField.text!)
-            //self.itemArray.append(Item(title: textField.text!, done: false)) // UserDafaults não aramazena array de objetos?
+            self.saveItems()
             
-            self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
-            // Recarrega os dados do array no tableView
-            self.tableView.reloadData()
             
         }
         alert.addTextField { (alertTextField) in
@@ -135,6 +103,36 @@ class TodoListViewController: UITableViewController {
         
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+    }
+    
+    
+    fileprivate func saveItems() {
+        
+        //self.defaults.set(self.itemArray, forKey: "TodoListArray")
+        
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(self.itemArray)
+            try data.write(to: self.dataFilePath!)
+        } catch {
+            print("Error encoding itemArray, \(error)")
+        }
+        // Recarrega os dados do array no tableView
+        self.tableView.reloadData()
+    }
+    
+    func loadItems() {
+        
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding itemArray, \(error)")
+            }
+        }
     }
     
 }
